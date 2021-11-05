8ccb8336962452276d0958f61bf21b622b8b601f
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-05 16:16:59 -0300

    'Canary Test' com 'failed'

--- src/fahrenheit/ConverterTest.java
+++ src/fahrenheit/ConverterTest.java
@@ -1,5 +1,17 @@
 package fahrenheit;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 public class ConverterTest {
+	
+	@Test
+	@DisplayName("Canary Test")
+	public void testOne(){
+
+		assertTrue(false);
+	}
 
 }
