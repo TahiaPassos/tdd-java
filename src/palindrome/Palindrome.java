b5909e121b9c7529daf8d6cd2e08f3dc8c211957
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-07 12:38:18 -0300

    Tratamento de string vazia passando

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -5,7 +5,7 @@ public class Palindrome {
 	public static Boolean isPalindrome(String phrase) {
 		
 	
-		if(phrase.equals("")) throw new IllegalArgumentException();
+		if(phrase.equals("")) throw new IllegalArgumentException("empty strings are not palindromes");
 		if(phrase.trim().equals("")) return false;
 
 		return phrase.toLowerCase().equals(new StringBuffer(phrase).reverse().toString());
