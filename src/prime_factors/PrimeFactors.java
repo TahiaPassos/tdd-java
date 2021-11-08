f2755505a441080d1b16f6a2ca1946d8e87ebf06
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-08 15:56:40 -0300

    Terceiro teste com 'succeed'

--- src/prime_factors/PrimeFactors.java
+++ src/prime_factors/PrimeFactors.java
@@ -2,19 +2,21 @@ package prime_factors;
 
 public class PrimeFactors {
 	
-	int [] factors;
+	int [] factors = new int[1];
 
 	public  Object findPrimeFactors(int primeNumbers) {
 				
 			if(primeNumbers == 1) {
 				return new int[0];
 			}
+			if(primeNumbers == 2)
+				factors[0] = 2;
 			
-			factors = new int[1];
-			factors[0] = 2;
+			if(primeNumbers == 3)
+				factors[0] = 3;
+			
 			
 			return factors[0];
 	}
 
-	
 }
--- src/prime_factors/PrimeFactorsTest.java
+++ src/prime_factors/PrimeFactorsTest.java
@@ -11,7 +11,7 @@ public class PrimeFactorsTest {
 	@DataProvider(name = "test prime factors")
 	public Object [][] provideNumbers(){
 //	return new Object[][] {{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}};
-	return new Object[][] {{2}};
+	return new Object[][] {{3}};
 	}
 		
 	
@@ -20,7 +20,8 @@ public class PrimeFactorsTest {
 		PrimeFactors primeFactors = new PrimeFactors();
 		
 //		assertEquals(new int[0], primeFactors.findPrimeFactors(primeNumbers));
-		assertEquals(2, primeFactors.findPrimeFactors(primeNumbers));
+//		assertEquals(2, primeFactors.findPrimeFactors(primeNumbers));
+		assertEquals(3, primeFactors.findPrimeFactors(primeNumbers));
 	}
 
 }
