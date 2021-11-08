647e9220cab51e6113076cd06797f6de050512f8
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-08 15:23:55 -0300

    Primeiro teste de 'prime factors'

---
+++ src/prime_factors/PrimeFactors.java
@@ -0,0 +1,11 @@
+package prime_factors;
+
+public class PrimeFactors {
+
+	public static int[] findPrimeFactors(Object primeNumbers) {
+
+		return new int[0];
+	}
+
+	
+}
--- src/prime_factors/PrimeFactorsTest.java
+++ src/prime_factors/PrimeFactorsTest.java
@@ -1,20 +1,22 @@
 package prime_factors;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 public class PrimeFactorsTest {
 	
-	@DataProvider(name = "test exceptions")
-	public Object [][] provideArgumentsToThrowException(){
+	@DataProvider(name = "test prime factors")
+	public Object [][] provideNumbers(){
 	return new Object[][] {{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}};
 	}
 		
 	
 	@Test(dataProvider = "test prime factors")
-	public void testFalse(Object primeNumbers){		
-		
+	public void testPrimeFa(Object primeNumbers){		
+		assertEquals(new int[0], PrimeFactors.findPrimeFactors(primeNumbers));
 	}
 
 }
