7cab198e468c01971ff375ff90769bc928815d38
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-08 17:18:28 -0300

    Código refatorado

--- src/prime_factors/PrimeFactorsTest.java
+++ src/prime_factors/PrimeFactorsTest.java
@@ -15,21 +15,25 @@ public class PrimeFactorsTest {
 	
 	@DataProvider(name = "test prime factors")
 	public Object [][] provideNumbers(){
-//	return new Object[][] {{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}};
-	return new Object[][] {{9}};
+	return new Object[][] {
+		{new ArrayList<Integer>(),1}, 
+		{new ArrayList<Integer>(Arrays.asList(2)),2}, 
+		{new ArrayList<Integer>(Arrays.asList(3)),3}, 
+		{new ArrayList<Integer>(Arrays.asList(2,2)),4}, 
+		{new ArrayList<Integer>(Arrays.asList(5)),5}, 
+		{new ArrayList<Integer>(Arrays.asList(2,3)),6}, 
+		{new ArrayList<Integer>(Arrays.asList(7)),7}, 
+		{new ArrayList<Integer>(Arrays.asList(2,2,2)),8}, 
+		{new ArrayList<Integer>(Arrays.asList(3,3)),9}};
 	}
-		
-	
+			
 	@Test(dataProvider = "test prime factors")
-	public void testPrimeFa(int primeNumbers){		
+	public void testPrimeFactors(List<Integer> expectedList, int primeNumbers){		
+		
 		PrimeFactors primeFactors = new PrimeFactors();
-
-		List<Integer> expectedArray = new ArrayList<Integer>(Arrays.asList(3,3));
 		List<Integer> actualArray = primeFactors.findPrimeFactors(primeNumbers);
 			
-			assertEquals(expectedArray, actualArray);
-
-
+		assertEquals(expectedList, actualArray);
 	}
 
 }
